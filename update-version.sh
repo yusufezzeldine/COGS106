a1b2108f1ad3f028f5fec6084275520d5ec259b6
yezzeldi <yezzeldi@uci.edu>
2023-01-20 23:23:10 -0800

    Moved files

---
+++ update-version.sh
@@ -0,0 +1,10 @@
+#Check that local repo is up to date with github
+git status
+
+#Something that saves the current date and time in a file
+date > /data/class/cogs106/yezzeldi/version
+
+#Update github with the new version file
+git add .
+git commit -m "Updated Version"
+git push --set-upstream origin main
