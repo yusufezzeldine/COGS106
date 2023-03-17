6ef384d073c3abee4cec1cbeef8592230b8b9738
yusufezzeldine <114191744+yusufezzeldine@users.noreply.github.com>
2023-03-17 02:07:43 -0700

    Revised private accept() method to mitigate division by 0

--- final_project.py
+++ final_project.py
@@ -17,8 +17,8 @@ class Metropolis:
         self.samples = []
 
     def __accept(self, proposal):
-        acceptance_prob = min(1, np.exp(self.logTarget(proposal) / self.logTarget(self.state)))
-        if np.random.uniform() < acceptance_prob:
+        acceptance_prob = min(0, (self.logTarget(proposal) - self.logTarget(self.state)))
+        if np.log(np.random.uniform()) < acceptance_prob:
             self.state = proposal
             return True
         return False
@@ -152,9 +152,71 @@ class SignalDetection:
             L.append(s.nLogLikelihood(predicted_hit_rate, s.FA()))
         return sum(L)
 
+import unittest
+
+class TestSignalDetection(unittest.TestCase):
+    def test_simulate(self):
+        # Test with a single criterion value
+        dPrime       = 1.5
+        criteriaList = [0]
+        signalCount  = 1000
+        noiseCount   = 1000
+
+        sdtList      = SignalDetection.simulate(dPrime, criteriaList, signalCount, noiseCount)
+        self.assertEqual(len(sdtList), 1)
+        sdt = sdtList[0]
+
+        self.assertEqual(sdt.hits             , sdtList[0].hits)
+        self.assertEqual(sdt.misses           , sdtList[0].misses)
+        self.assertEqual(sdt.false_alarms      , sdtList[0].false_alarms)
+        self.assertEqual(sdt.correct_rejections, sdtList[0].correct_rejections)
+
+        # Test with multiple criterion values
+        dPrime       = 1.5
+        criteriaList = [-0.5, 0, 0.5]
+        signalCount  = 1000
+        noiseCount   = 1000
+        sdtList      = SignalDetection.simulate(dPrime, criteriaList, signalCount, noiseCount)
+        self.assertEqual(len(sdtList), 3)
+        for sdt in sdtList:
+            self.assertLessEqual    (sdt.hits              ,  signalCount)
+            self.assertLessEqual    (sdt.misses            ,  signalCount)
+            self.assertLessEqual    (sdt.false_alarms       ,  noiseCount)
+            self.assertLessEqual    (sdt.correct_rejections ,  noiseCount)
+
+    def test_nLogLikelihood(self):
+        sdt = SignalDetection(10, 5, 3, 12)
+        hit_rate = 0.5
+        false_alarm_rate = 0.2
+        expected_nll = - (10 * np.log(hit_rate) +
+                           5 * np.log(1-hit_rate) +
+                           3 * np.log(false_alarm_rate) +
+                          12 * np.log(1-false_alarm_rate))
+        self.assertAlmostEqual(sdt.nLogLikelihood(hit_rate, false_alarm_rate),
+                               expected_nll, places=6)
+
+    def test_rocLoss(self):
+        sdtList = [
+            SignalDetection( 8, 2, 1, 9),
+            SignalDetection(14, 1, 2, 8),
+            SignalDetection(10, 3, 1, 9),
+            SignalDetection(11, 2, 2, 8),
+        ]
+        a = 0
+        expected = 99.3884206555698
+        self.assertAlmostEqual(SignalDetection.rocLoss(a, sdtList), expected, places=4)
+
+    def test_integration(self):
+        dPrime = 1
+        sdtList = SignalDetection.simulate(dPrime, [-1, 0, 1], 1e7, 1e7)
+        aHat = SignalDetection.fit_roc(sdtList)
+        self.assertAlmostEqual(aHat, dPrime, places=2)
+        plt.close()
+
+if __name__ == '__main__':
+    unittest.main(argv=['ignored'], exit=False)
+
 import scipy.stats
-import numpy as np
-import matplotlib.pyplot as plt
 
 def fit_roc_bayesian(sdtList):
 
